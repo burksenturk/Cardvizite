f37c2e91757c841fc3ac4c57b94c4ab7c1f8a4ed
Burak Senturk <senturkburk@gmail.com>
2024-02-04 18:13:27 +0300

    Add project files.

---
+++ Cardvizite/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Cardvizite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cardvizite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
